0f362e10430430d088feeadb16e17b61f8c89b0e
Jared Stromberg <jaredstromberg@gmail.com>
2019-03-05 03:57:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 def my_each(array) # put argument(s) here
+=======
+def my_each(array)
+>>>>>>> 510aedae628d82fcf0f87ac165dcdfc9abad5ce9
   i = 0
   while i < array.length
     yield array[i]
